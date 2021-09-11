ddc2121c422c76b4f69d62e11bbf0e0a5a3c6a83
Oleg <super.vec2013@gmail.com>
2021-09-12 02:00:44 +0300

    Добавьте файлы проекта.

---
+++ SCP173Rework/Handler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Exiled.API.Enums;
+
+namespace SCP173Rework
+{
+    public class Handler
+    {
+        public void OnBlinking(BlinkingEventArgs ev)
+        {
+            if (ev.Player.Role == RoleType.Scp173)
+                if (ev.Targets.Count >= 4)
+                {
+                    ev.Player.EnableEffect(EffectType.Ensnared,2);
+                    ev.Player.ShowHint("На вас смотрят <color=red>{int}</color> человек, вы не можете двигаться".Replace("{int}", ev.Targets.Count.ToString()), 5);
+                }
+                else
+                    ev.Player.ShowHint("На вас смотрят <color=red>{int}</color> человек".Replace("{int}", ev.Targets.Count.ToString()), 5);
+        }
+    }
+}
---
+++ SCP173Rework/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Events = Exiled.Events.EventArgs;
+using Scp173 = Exiled.Events.Handlers.Scp173;
+
+namespace SCP173Rework
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name { get; } = "SCP173Rework";
+        public override string Author { get; } = ".fkn_goose";
+        public override Version Version => new Version(0, 0, 1);
+        public static readonly Lazy<Plugin> LazyInstance = new Lazy<Plugin>(valueFactory: () => new Plugin());
+        public static Plugin PluginItem => LazyInstance.Value;
+        private Handler handler;
+        public override void OnEnabled()
+        {
+            handler = new Handler();
+            Scp173.Blinking += handler.OnBlinking;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Scp173.Blinking -= handler.OnBlinking;
+            handler = null;
+            base.OnDisabled();
+        }
+    }
+}
